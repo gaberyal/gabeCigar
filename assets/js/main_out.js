cd5d2395b2e635bc16b571e0e1a77d6f6a39cfcc
Gabe Ryal <86553565+gaberyal@users.noreply.github.com>
2023-10-09 20:14:22 +0200

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1670,7 +1670,7 @@
         byId('play-btn').addEventListener('click', () => {
             const secretCode = document.getElementById('code-secret').value;
 
-            if (secretCode !== "macrogod") {
+            if (secretCode !== "aap future") {
                 alert("Wrong access code. You can not access the server. Please join the discord to get the current access code");
                 return;
             }
