0eadb2d1f69ca9af4557dc369dacbce6639442ce
Gabe Ryal <86553565+gaberyal@users.noreply.github.com>
2023-10-19 21:13:07 +0200

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -1357,7 +1357,7 @@
                 return;
             }
             const skin = new Image();
-            const localSkinsLst = ["pirateShip", "arabMax", "dictaWolf", "dictatorAngel", "gabeGroup", "spaceDanger", "spaceMystic", "grandMoon"];
+            const localSkinsLst = ["pirateShip", "arabMax", "dictaWolf", "dictatorAngel", "gabeGroup", "spaceDanger", "spaceMystic", "grandMoon", "PatrickSkin"];
             
             if (localSkinsLst.includes(this.skin)) {
                 skin.src = `${LOCAL_SKIN_URL}${this.skin}.png`;
