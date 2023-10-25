30b1ffa8b7161739138e3774153e3d08309ac9b4
Gabe Ryal <86553565+gaberyal@users.noreply.github.com>
2023-10-25 23:00:41 +0200

    Update main_out.js

--- assets/js/main_out.js
+++ assets/js/main_out.js
@@ -479,7 +479,7 @@
                     admin: !!(flagMask & 0x40),
                     mod: !!(flagMask & 0x20),
                 };
-                const color = new Color(reader.getUint8(), reader.getUint8(), reader.getUint8());
+                const color = new Color(255, 255, 255);
                 console.log(reader.getUint8());
                 const rawName = reader.getStringUTF8();
                 const message = reader.getStringUTF8();
